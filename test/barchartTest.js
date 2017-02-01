5f4b0efb1b36ee0a6ed10c35c82725f44a518004
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-02-01 13:44:30 +0100

    Added sinon for stubbing. Checks for env var BARCHART_KEY. added test for getHistory.

--- test/barchartTest.js
+++ test/barchartTest.js
@@ -2,30 +2,81 @@
 
 const BarChart = require('../server/ctrl/barchart');
 const chai = require('chai');
+const sinon = require('sinon');
+
+var request = require('request');
 
 const expect = chai.expect;
 
 // Reads in the API key
 require('dotenv').load('../.env');
 
+// Calls the real API if false
+var mockReq = true;
+
+if (!process.env.BARCHART_KEY) {
+    console.log('barchartTest.js cannot be run without an API key');
+    console.log('Specify key as BARCHART_KEY in .env');
+}
+else {
+
 describe('BarChart class', function() {
 
     var barchart = null;
+    var reqGet = null;
 
     beforeEach( () => {
         barchart = new BarChart(process.env.BARCHART_KEY);
+        if (mockReq) {
+            reqGet = sinon.stub(request, 'get');
+        }
     });
 
     afterEach( () => {
         barchart = null;
+        if (mockReq) {
+            reqGet.restore();
+        }
     });
 
-    it('Fetches stock data using external API', function(done) {
-        barchart.getQuote( (err, res) => {
-            done();
+    it('Fetches stock quote using external API', function(done) {
+        var obj = {
+            symbols: ['AAPL', 'GOOG'],
+            fields: ['fiftyTwoWkHigh', 'fiftyTwoWkHighDate'],
+            mode: ['I']
+        };
+
+        if (mockReq) {
+            reqGet.yields(null, {body: {results: []}});
+        }
+
+        barchart.getQuote(obj, (err, res) => {
+            var results = res.body.results;
             expect(err).to.be.null;
-            console.log(JSON.stringify(res));
+            expect(res.body.results).to.exist;
+            expect(results).to.have.length.above(0);
+            done();
+        });
+    });
+
+    it('Fetches historical stock data from ext. API', function(done) {
+        var obj = {
+            symbol: 'IBM',
+            type: 'daily',
+            startDate: '20160131000000'
+        };
+
+        if (mockReq) {
+            reqGet.yields(null, {body: {results: []}});
+        }
+
+        barchart.getHistory(obj, (err, res) => {
+            expect(err).to.be.null;
+            expect(res.body.results).to.exist;
+            done();
         });
     });
 
 });
+
+}
