3c5019bf8d45cdf83d21a97b692ad7e86c8ca499
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-02-05 17:09:51 +0100

    On socket connection, server sends existing stockData to the client.

--- server.js
+++ server.js
@@ -49,22 +49,40 @@ app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.locals.pretty = true;
 
-
 app.use(morgan('combined'));
 
 routes(app);
 
 var port = process.env.PORT || 8080;
 
+// Used only for testing, before DB is connected
+var stockData = {};
+
 http.listen(port, function() {
 	console.log('StockChart Server listening on port ' + port + '...');
 });
 
+
 io.on('connection', (socket) => {
     console.log('A user connected.');
 
-    socket.on('client message', (msg) => {
-        console.log('Server got message: ' + msg);
+    var symbols = Object.keys(stockData);
+    symbols.forEach( (item) => {
+        var msg = {body: stockData[item], cmd: 'addSym'};
+        msg.symbol = item;
+        socket.emit('server message', msg);
+
+    });
+
+
+    socket.on('client message', processClientMsg);
+});
+
+
+/* Process addSym/delSym commands from clients.*/
+function processClientMsg(msg) {
+    console.log('Server got message: ' + msg);
+    if (msg.cmd === 'addSym') {
         var query = {
             symbol: msg.symbol,
             type: 'daily',
@@ -76,12 +94,24 @@ io.on('connection', (socket) => {
                 io.emit('server message', {error: 'An error occurred.'});
             }
             else {
-                var newMsg = Object.assign({}, msg, {newMsg: 'Server says hi'});
+                var newMsg = Object.assign({}, msg);
                 newMsg.body = body;
                 io.emit('server message', newMsg);
+                stockData[msg.symbol] = newMsg.body;
             }
 
         });
-    });
-});
+    }
+    else {
+        // Emit the delSym without modifications
+        var symbol = msg.symbol;
+        if (stockData.hasOwnProperty(symbol)) {
+            io.emit('server message', msg);
+            delete stockData[symbol];
+        }
+        else {
+            console.error('Symbol ' + symbol + ' does not exist.');
+        }
+    }
 
+}
