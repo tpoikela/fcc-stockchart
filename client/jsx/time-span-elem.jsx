dfba56081c36283258cb6220d13989e297489154
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-02-12 10:55:42 +0100

    Shows now active button using btn-success.

--- client/jsx/time-span-elem.jsx
+++ client/jsx/time-span-elem.jsx
@@ -3,16 +3,39 @@ const React = require('react');
 
 class TimeSpanElem extends React.Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+            selected: '1y'
+        };
+
+        this.buttons = ['1m', '3m', '6m', '1y'];
+
+        this.onClick = this.onClick.bind(this);
+    }
+
+    onClick(e) {
+        this.props.onClick(e);
+        var text = e.target.textContent;
+        this.setState({selected: text});
+    }
 
     render() {
-        var onClick = this.props.onClick;
+        var onClick = this.onClick;
+
+        var buttons = this.buttons.map( (item) => {
+            var className = 'btn-primary';
+            if (this.state.selected === item) {
+                className = 'btn-success';
+            }
+            return (
+                <button className={className} onClick={onClick}>{item}</button>
+            );
+        });
 
         return (
             <div className='time-span-elem'>
-                <button onClick={onClick}>1m</button>
-                <button onClick={onClick}>3m</button>
-                <button onClick={onClick}>6m</button>
-                <button onClick={onClick}>1y</button>
+                {buttons}
             </div>
         );
 
