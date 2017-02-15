94c31a7d93875f435fb3752aab91536d0c73ac81
Tuomas Poikela <tuomas.sakari.poikela@gmail.com>
2017-02-15 13:30:07 +0100

    Moved all socket logic into specific controller.

--- server.js
+++ server.js
@@ -23,6 +23,7 @@ var express = require('express');
 var mongoose = require('mongoose');
 var morgan = require('morgan');
 const BarChart = require('./server/ctrl/barchart');
+const SocketCtrl = require('./server/ctrl/socket-ctrl');
 
 // Sources for this app
 var routes = require('./server/routes/index.js');
@@ -34,8 +35,6 @@ var io = require('socket.io')(http);
 
 var barchart = new BarChart(process.env.BARCHART_KEY);
 
-app.set('view engine', 'pug');
-
 app.url = process.env.APP_URL;
 console.log('The full APP url: ' + app.url);
 
@@ -45,7 +44,6 @@ mongoose.Promise = global.Promise;
 // Initialize resource paths for the server
 app.use('/build', express.static(process.cwd() + '/build'));
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use('/common', express.static(process.cwd() + '/app/common'));
 
 app.locals.pretty = true;
 
@@ -62,85 +60,6 @@ http.listen(port, function() {
 	console.log('StockChart Server listening on port ' + port + '...');
 });
 
+var socketCtrl = new SocketCtrl(io, barchart, stockData);
+socketCtrl.verbosity = 0;
 
-io.on('connection', (socket) => {
-    console.log('A user connected.');
-
-    var symbols = Object.keys(stockData);
-    symbols.forEach( (item) => {
-        var msg = {body: stockData[item], cmd: 'addSym'};
-        msg.symbol = item;
-        socket.emit('server message', msg);
-
-    });
-
-
-    socket.on('client message', processClientMsg);
-});
-
-
-/* Process addSym/delSym commands from clients.*/
-function processClientMsg(msg) {
-    console.log('Server got message: ' + JSON.stringify(msg));
-    if (msg.cmd === 'addSym') {
-        var startDate = getStartDate(372);
-        var query = {
-            symbol: msg.symbol,
-            type: 'daily',
-            startDate: startDate
-        };
-
-        barchart.getHistory(query, (err, res, body) => {
-            if (err) {
-                io.emit('server message', {error: 'An error occurred.'});
-            }
-            else {
-                var bodyObj = JSON.parse(body);
-                console.log(JSON.stringify(body));
-                console.log('Response status.code: ' + bodyObj.status.code);
-                var code = bodyObj.status.code;
-                if (code === 200) {
-                    var newMsg = Object.assign({}, msg);
-                    newMsg.body = body;
-                    io.emit('server message', newMsg);
-                    stockData[msg.symbol] = newMsg.body;
-                }
-                else if (code === 204) {
-                    var errorMsg = {error: 'No symbol ' + msg.symbol
-                        + ' found.'};
-                    io.emit('server message', errorMsg );
-                }
-            }
-
-        });
-    }
-    else {
-        // Emit the delSym without modifications
-        var symbol = msg.symbol;
-        if (stockData.hasOwnProperty(symbol)) {
-            io.emit('server message', msg);
-            delete stockData[symbol];
-        }
-        else {
-            console.error('Symbol ' + symbol + ' does not exist.');
-        }
-    }
-
-}
-
-/* Returns date N days from today. */
-function getStartDate(nDays) {
-    var dateNow = new Date();
-    var dateStartMs = dateNow.getTime() - 1000 * 3600 * 24 * nDays;
-    var dateStart = new Date(dateStartMs);
-    var year = '' + dateStart.getFullYear();
-    var month = (dateStart.getMonth() + 1);
-    if (month < 10) {
-        month = '0' + month;
-    }
-    var day = '' + dateStart.getDate();
-    var result = year + month + day + '000000';
-    console.log('getStartDate result: ' + result);
-    // return '20160101000000';
-    return result;
-}
